6d1798ed5d52810a1feedf7dd43bf6d3ebab3baf
ana-carolina <anacdomenicale@gmail.com>
2022-08-31 15:12:51 -0300

    Atendimento

--- api/index.js
+++ api/index.js
@@ -4,8 +4,7 @@ var mongoose = require('mongoose');
 
 //conecxão com banco de dados
 mongoose.Promisse = global.Promisse;
-mongoose.connect('mongobd://localhost:27017/projeto-web-fullstack',
-    { useMongoClient: true })
+mongoose.connect('mongodb://localhost:27017/projeto-web-fullstack')
     .then(() => {
         console.log("conexão realizada com sucesso!");
     })
